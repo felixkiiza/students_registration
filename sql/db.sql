4fb3b15f991e25b267210a879bce4158ff027535
felixkiiza <felixkiiza@gmail.com>
2020-06-05 13:40:42 +0300

    added db file

---
+++ sql/db.sql
@@ -0,0 +1,28 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `regno` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `dept_name` varchar(200) NOT NULL,
+  `course_name` varchar(200) NOT NULL,
+  `sem_no` varchar(100) NOT NULL,
+  `year` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
